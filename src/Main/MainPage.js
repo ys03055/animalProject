e37ae59d6745ecff7717ed3f8f3b260ccb3f1e64
ys03055 <ys03055@naver.com>
2022-09-25 15:48:01 +0900

    메인페이지 업데이트2

--- src/Main/MainPage.js
+++ src/Main/MainPage.js
@@ -1,10 +1,15 @@
 import React, {Fragment, useEffect, useState} from "react";
 import { Card } from 'antd';
 import "./MainPage.css";
-import HeaderPage from "../Header/HeaderPage"
+import HeaderPage from "../Header/HeaderPage";
+import dog from "../pic/dog.jpg";
+import cat from "../pic/cat.jfif"
+import deer from "../pic/deer.jfif"
+import snake from "../pic/snake.jfif"
 import {Link} from "react-router-dom";
 
 function MainPage()  {
+
 
 
     return (
@@ -20,52 +25,70 @@ function MainPage()  {
 
             <main className="Main">
                 <div>
-                <div className="MainPageMiniHeader"> </div>
                 <div className="MainPageCardDiv">
-                    <Card title="내 반려동물과의 하루">
-                    <Card.Grid className="MainPageCard">Content</Card.Grid>
-                    <Card.Grid className="MainPageCard">Content</Card.Grid>
-                    <Card.Grid className="MainPageCard">Content</Card.Grid>
-                    <Card.Grid className="MainPageCard">Content</Card.Grid>
+                    <Card className="MainPageMiniHeader">
+                        *나의 반려동물을 위한 용품 추천
                     </Card>
+                        <Card hoverable className="MainPageCard" cover = {<img className="Pic" src={dog} />}>
+                        </Card>
+                        <Card hoverable className="MainPageCard" cover = {<img className="Pic" src={cat} />}>
+                        </Card>
+                        <Card hoverable className="MainPageCard" cover = {<img className="Pic" src={deer} />}>
+                        </Card>
+                        <Card hoverable className="MainPageCard" cover = {<img className="Pic" src={snake} />}>
+                        </Card>
                 </div>
 
-
-                <div className="MainPageMiniHeader"></div>
                 <div className="MainPageCardDiv">
-                    <Card title="나의 반려동물을 위한 용품 추천" className="MainPageMiniHeader">
-                    <Card.Grid className="MainPageCard">Content</Card.Grid>
-                    <Card.Grid className="MainPageCard">Content</Card.Grid>
-                    <Card.Grid className="MainPageCard">Content</Card.Grid>
-                    <Card.Grid className="MainPageCard">Content</Card.Grid>
+                    <Card className="MainPageMiniHeader" >
+                        *나의 반려동물을 위한 용품 추천
                     </Card>
+                        <Card hoverable className="MainPageCard" cover = {<img className="Pic" src={dog} />}>
+                        </Card>
+                        <Card hoverable className="MainPageCard" cover = {<img className="Pic" src={cat} />}>
+                        </Card>
+                        <Card hoverable className="MainPageCard" cover = {<img className="Pic" src={deer} />}>
+                        </Card>
+                        <Card hoverable className="MainPageCard" cover = {<img className="Pic" src={snake} />}>
+                        </Card>
                 </div>
 
-                <div className="MainPageMiniHeader"></div>
                 <div className="MainPageCardDiv">
-                    <Card title="이 주의 반려동물">
-                    <Card.Grid className="MainPageCard">Content</Card.Grid>
-                    <Card.Grid className="MainPageCard">Content</Card.Grid>
-                    <Card.Grid className="MainPageCard">Content</Card.Grid>
-                    <Card.Grid className="MainPageCard">Content</Card.Grid>
-                    </Card>
+                    <Card className="MainPageMiniHeader" >
+                        *이 주의 반려동물
+                        </Card>
+                        <Card hoverable className="MainPageCard" cover = {<img className="Pic" src={dog} />}>
+                        </Card>
+                        <Card hoverable className="MainPageCard" cover = {<img className="Pic" src={cat} />}>
+                        </Card>
+                        <Card hoverable className="MainPageCard" cover = {<img className="Pic" src={deer} />}>
+                        </Card>
+                        <Card hoverable className="MainPageCard" cover = {<img className="Pic" src={snake} />}>
+                        </Card>
                 </div>
 
-                <div className="MainPageMiniHeader"></div>
                 <div className="MainPageCardDiv">
-                    <Card title="명예 회원">
-                    <Card.Grid className="MainPageCard">Content</Card.Grid>
-                    <Card.Grid className="MainPageCard">Content</Card.Grid>
-                    <Card.Grid className="MainPageCard">Content</Card.Grid>
-                    <Card.Grid className="MainPageCard">Content</Card.Grid>
+                    <Card className="MainPageMiniHeader" >
+                        *명예 회원
                     </Card>
+                        <Card hoverable className="MainPageCard" cover = {<img className="Pic" src={dog} />}>
+                        </Card>
+                        <Card hoverable className="MainPageCard" cover = {<img className="Pic" src={cat} />}>
+                        </Card>
+                        <Card hoverable className="MainPageCard" cover = {<img className="Pic" src={deer} />}>
+                        </Card>
+                        <Card hoverable className="MainPageCard" cover = {<img className="Pic" src={snake} />}>
+                        </Card>
                 </div>
 
                 <Card className="honeyBoards">
                 <h1 className="honeyBoardHeader">꿀팁 게시판</h1>
-                    <Link to ="/">더보기</Link>
-                    <br/>
                 <div className="honeyBoardDiv">
+                    <div className="linkStyle">
+                        <Link to ="/">더보기</Link>
+                    </div>
+                    <br/>
+                    <br/>
                     <Link to ="/">강아지를 길들이는 방법</Link>
                     <br/>
                     <br/>
@@ -84,9 +107,12 @@ function MainPage()  {
 
                     <Card className="freeBoards">
                         <h1 className="freeBoardHeader">자유 게시판</h1>
-                        <Link to ="/">더보기</Link>
-                        <br/>
                         <div className="freeBoardDiv">
+                            <div className="linkStyle">
+                                <Link to ="/">더보기</Link>
+                            </div>
+                            <br/>
+                            <br/>
                             <Link to ="/">강아지가 말을 안들어요 ㅠ</Link>
                             <br/>
                             <br/>
